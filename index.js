c5aafa9cb55817fa8d937bf81579d47de8b4ad85
kbc6s <f129546497@gmail.com>
2019-03-05 14:05:45 +0800

    add line function

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var request = require("request");
 const express = require('express')
 var app = express()
-
 var Token = "ohMIiD5N1vx7mlzlUWwb9EqcMY533cGBI6y8EPxbGOb"
 
-
+//lineNotify function
+var lineNotify = function (place,event){
+    var options = {
+        method: 'POST',
+        url: 'https://notify-api.line.me/api/notify',
+        headers: {
+            Authorization: `Bearer ${Token}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+            message: "\n" + '地點：' + place + "\n" + "事件: " + event
+        }
+    };
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        console.log(body);
+    });
+}
 //定時檢查資料
 
 var myInt = setInterval(function () {
@@ -22,6 +38,7 @@ var myInt = setInterval(function () {
 
 app.get("/HWC", function (req, res) {
     res.send("hello world333!!!!!")
+    lineNotify("7697","測試中")
 })
 
 app.listen(3001, function () {
