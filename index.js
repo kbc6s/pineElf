8c940dca93a950eee483a16f5de07918e6ea26f8
kbc6s <f129546497@gmail.com>
2019-04-21 10:16:58 +0800

    123124

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//water Tower
 var request = require("request");
 const express = require('express')
 var app = express()
@@ -5,28 +6,43 @@ var Token = "ohMIiD5N1vx7mlzlUWwb9EqcMY533cGBI6y8EPxbGOb"
 var http = require('http')
 
 //JSON parse method
-var GetInflux = {
-    method: 'GET',
-    url: 'http://kaiwen1995.com:8086/query?db=lineBot;q=select * from lineBot where LineID=\'U8168367ec76c449dbdd98410d9333b8\'',
-    // headers: {
-    //     CK: 'DK4TSU4BPWTWWFW5EC'
-    // }
-};
-app.get("/", function (req, res) {
-    request(GetInflux, function (error, response, body) {
-        if (error) throw new Error(error);
-        // console.log(typeof(body))
-        var kaibuff = JSON.parse(body);
+// var GetInflux = {
+//     method: 'GET',
+//     url: 'http://kaiwen1995.com:8086/query?db=lineBot;q=select * from lineBot where LineID=\'U8168367ec76c449dbdd98410d9333b8\'',
+// };
+// app.get("/", function (req, res) {
+//     request(GetInflux, function (error, response, body) {
+//         if (error) throw new Error(error);
+//         // console.log(typeof(body))
+//         var kaibuff = JSON.parse(body);
       
-        var stringResponse = JSON.stringify(kaibuff.results[0].series[0].values[0][0])
-        res.send(kaibuff);
+//          var stringResponse = JSON.stringify(kaibuff.results[0].series[0].values[0][3])
+//         // var stringResponse = JSON.stringify(kaibuff.results[0])
+//         res.send(kaibuff);
         
-        console.log(stringResponse);
+//         console.log(stringResponse);
         
-    })
-})
+//     })
+// })
     
+var options = { method: 'POST',
+  url: 'http://192.168.3.69/WaWebService/Json/GetTagValue/Leegood',
+  headers: 
+   { Authorization: 'Basic YWRtaW46bGVlZ29vZA==',
+     'Content-Type': 'application/json' },
+  body: { Tags: [ { Name: 'DO1' } ] },
+  json: true };
 
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  //console.log(body);
+  //var qqq = JSON.parse(body);
+  var qqq = body;
+   console.log(qqq.Values[0].Value);
+
+  var int = 1;
+});
 //lineNotify function
 var lineNotify = function (place,event){
     var options = {
