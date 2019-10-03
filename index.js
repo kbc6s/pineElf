5b673417b5954e5d433c16534cb5009f501ec663
kbc6s <f129546497@gmail.com>
2019-10-03 17:15:37 +0800

    change Token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var request = require("request");
 const express = require('express')
 var app = express()
-var Token = "ohMIiD5N1vx7mlzlUWwb9EqcMY533cGBI6y8EPxbGOb"
+var Token = "dDzOGwQdOB35Oru3VnaWJ3ssEJQFz9gfxtrWMbdNBmf"
 var http = require('http')
 
 var waterTower
